ed49877e3f14c1906ce72329f065b439dd737014
John George <68780394+johngeorge88@users.noreply.github.com>
2020-12-13 11:46:42 -0500

    adding the user model

---
+++ models/User.js
@@ -0,0 +1,41 @@
+const { Schema, model } = require('mongoose');
+// const dateFormat = require('../utils/dateFormat');
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: "You must enter a username",
+        trim: true
+    },
+    email: {
+        type: String,
+        required: "E-mal address is required",
+        unique: true,
+        match: [/.+\@.+\..+/, "Please enter a valid e-mail address"],
+    },
+    thoughts: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+    }],
+    friends: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+}, {
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    // prevents virtuals from creating duplicate of _id as `id`
+    id: false
+});
+
+// get total count of friends
+UserSchema.virtual("friendCount").get(function() {
+    return this.friends.length;
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
